54691037da710ae3980b7106c4889008213eeca2
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-05-16 11:06:43 +0100

    statement added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
 
@@ -6,19 +7,19 @@ function App() {
     name: "Connor Fleming",
     location: "Glasgow, Scotland",
     email: "connor.fleming1992@gmail.com",
-    availability: "Open to work"
+    availability: "Open to work",
+    statement: "Junior software developer driven by the rush and exhilaration that comes with building fullstack applications, bug fixes, and solving technical issues.",
   };
 
   const socialMedia = {
     linkedIn: "https://www.linkedin.com/in/connor-j-fleming/",
     gitHub: "https://github.com/Jarnbjorn92",
-    
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        Connor's portfolio
+        <HomePage/>
       </header>
     </div>
   );
