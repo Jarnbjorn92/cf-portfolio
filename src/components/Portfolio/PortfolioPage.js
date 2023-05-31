45dcebf2e2679f107bede443bd209cf32d5e7bb0
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-05-31 15:32:11 +0100

    portfolio work

--- src/components/Portfolio/PortfolioPage.js
+++ src/components/Portfolio/PortfolioPage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import portfolioData from './portfolioData.json';
+import Project from './Project';
+
 
 const PortfolioPage = () => {
 
+    const portfolioNodes = portfolioData.map((project, index) => {
+
+        return <Project 
+            key={index}
+            id={project.id}
+            title={project.title}
+            technologies={project.technologies}
+            image={project.image}
+            color={project.bgcolor}
+            github={project.github}
+            deployed={project.deployed}
+            description={project.description} />
+        });
+
     return (
-        <div>PortfolioPage</div>
+        <div>{portfolioNodes}</div>
     );
 };
 
