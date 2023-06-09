0f103116a2241c15d8de58473aef1018eca42209
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-06-09 11:00:02 +0100

    media query about and drop shadow footer

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -20,7 +20,7 @@ const AboutPage = () => {
         <motion.div
             className="aboutInfo"
             ref={ref}
-            initial={{ x: "25vw", opacity: 0 }}
+            initial={{ x: "10vw", opacity: 0 }}
             animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
             transition={{ duration: 0.5, ease: "easeInOut" }}
             >
