a416d3d57a12aa0db2e8c8f806f9c962ec4ae27e
Connor Fleming <117941613+Jarnbjorn92@users.noreply.github.com>
2023-06-12 12:41:41 +0100

    download working but cannot find pdf

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,29 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import NavBar from '../NavBar';
 import SocialLinks from './SocialLinks';
 import './ContactPage.css';
-import cv from './assets/Connor_fleming_software_developer.pdf'
+import cv from './assets/Connor_fleming_software_developer.pdf';
 
-const ContactPage = ({personalDetails}) => {
-
+const ContactPage = ({ personalDetails }) => {
     const [ref, inView] = useInView({
         threshold: 0,
         triggerOnce: true,
     });
 
+    // Subit Success State
     const [success, setSuccess] = useState(false);
-    const [download, setDownload] = useState(false);
-
+    // Form Submit Data
     const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        subject: "",
-        message: "",
-        access_key: "534eb692-031e-4304-9b5e-f7c757958921",
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        access_key: '534eb692-031e-4304-9b5e-f7c757958921',
     });
-
+    //Form handle data
     const handleChange = (event) => {
         setFormData({
         ...formData,
@@ -36,24 +35,11 @@ const ContactPage = ({personalDetails}) => {
 
     const data = JSON.stringify(formData);
 
-    const handleDownload = () => {
-        setDownload(true);
-        const link = document.createElement("a");
-        link.href = cv;
-        link.download = "Connor_Fleming_CV.pdf";
-        link.onload = () => {
-            link.remove();
-            setDownload(false);
-        };
-        document.body.appendChild(link);
-        link.click();
-    };
-
-    fetch("https://api.web3forms.com/submit", {
-        method: "POST",
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
         headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
         },
         body: data,
     })
@@ -62,109 +48,127 @@ const ContactPage = ({personalDetails}) => {
             setSuccess(true);
             setFormData({
             ...formData,
-            name: "",
-            email: "",
-            subject: "",
-            message: "",
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
         });
         setTimeout(() => {
             setSuccess(false);
         }, 3000);
     })
-    .catch((err) => console.log(err));
-};
+        .catch((err) => console.log(err));
+    };
 
+    // Download CV Function
+    const handleDownload = () => {
+        const filename = 'Connor_fleming_software_developer.pdf';
+        const filePath = './assets/Connor_fleming_software_developer.pdf';
+
+        // Create a temporary anchor element
+        const link = document.createElement('a');
+        link.href = filePath;
+        link.download = filename;
+
+        // Append the anchor to the body and trigger the download
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up the temporary anchor
+        document.body.removeChild(link);
+    };
+    
+    //Fade in framer variant
     const fadeInVariants = {
         hidden: {
-            opacity: 0,
+        opacity: 0,
         },
         visible: {
-            opacity: 1,
-            transition: {
-                delay: 0.8,
-                duration: 0.5,
-            },
+        opacity: 1,
+        transition: {
+            delay: 0.8,
+            duration: 0.5,
+        },
         },
     };
 
-
     return (
-        <div className='ContactPage'>
-        <NavBar/>
-        
-        <section className='contactSection'>
-        <motion.form
-        action=""
-        ref={ref}
-        className="contactForm"
-        initial={{ x: "-10vw", opacity: 0 }}
-        animate={inView ? { x: 0, opacity: 1 } : { x: "-10vw", opacity: 0 }}
-        transition={{ duration: 0.4, ease: "easeInOut" }}
-        onSubmit={handleSubmit}
-        >
+        <div className="ContactPage">
+            <NavBar />
 
-        <h4 className="contentTitle">Message Me</h4>
-        <div className="formGroup" style={{ display: "inline-block", margin: "5px" }}>
-        <input
-            type="text"
-            className="formControl"
-            onChange={handleChange}
-            value={formData.name}
-            id="contactName"
-            name="name"
-            placeholder="Name"
-            required
-        />
-        </div>
-        <div className="formGroup" style={{ display: "inline-block" }}>
-        <input
-            type="email"
-            className="formControl"
-            onChange={handleChange}
-            value={formData.email}
-            id="contactEmail"
-            name="email"
-            placeholder="Email"
-            required
-        />
-        </div>
-        <div className="col-12 formGroup">
-        <input
-            type="text"
-            className="formControl"
-            onChange={handleChange}
-            value={formData.subject}
-            id="contactSubject"
-            name="subject"
-            placeholder="Subject"
-            required
-        />
-        </div>
-        <div className="col-12 formGroup">
-        <textarea
-            className="formControl"
-            onChange={handleChange}
-            value={formData.message}
-            name="message"
-            id="contactMessage"
-            rows="12"
-            placeholder="Message"
-            required
-        ></textarea>
-        </div>
-        <div className="col-12 formGroup formSubmit">
-        <button className="btn">{success ? "Message Sent" : "Send Message"}</button>
-        </div>
+        <section className="contactSection">
+        <motion.form
+            action=""
+            ref={ref}
+            className="contactForm"
+            initial={{ x: '-10vw', opacity: 0 }}
+            animate={inView ? { x: 0, opacity: 1 } : { x: '-10vw', opacity: 0 }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+            onSubmit={handleSubmit}
+        >
+            <h4 className="contentTitle">Message Me</h4>
+            <div className="formGroup" style={{ display: 'inline-block', margin: '5px' }}>
+                <input
+                type="text"
+                className="formControl"
+                onChange={handleChange}
+                value={formData.name}
+                id="contactName"
+                name="name"
+                placeholder="Name"
+                required
+                />
+            </div>
+            <div className="formGroup" style={{ display: 'inline-block' }}>
+                <input
+                type="email"
+                className="formControl"
+                onChange={handleChange}
+                value={formData.email}
+                id="contactEmail"
+                name="email"
+                placeholder="Email"
+                required
+                />
+            </div>
+            <div className="col-12 formGroup">
+                <input
+                type="text"
+                className="formControl"
+                onChange={handleChange}
+                value={formData.subject}
+                id="contactSubject"
+                name="subject"
+                placeholder="Subject"
+                required
+                />
+            </div>
+            <div className="col-12 formGroup">
+                <textarea
+                className="formControl"
+                onChange={handleChange}
+                value={formData.message}
+                name="message"
+                id="contactMessage"
+                rows="12"
+                placeholder="Message"
+                required
+                ></textarea>
+            </div>
+
+            <div className="col-12 formGroup formSubmit">
+                <button className="btn">{success ? 'Message Sent' : 'Send Message'}</button>
+            </div>
         </motion.form>
 
         <motion.div
             className="contactInfo"
             ref={ref}
-            initial={{ x: "10vw", opacity: 0 }}
-            animate={inView ? { x: 0, opacity: 1 } : { x: "10vw", opacity: 0 }}
-            transition={{ duration: 0.4, ease: "easeInOut" }}
+            initial={{ x: '10vw', opacity: 0 }}
+            animate={inView ? { x: 0, opacity: 1 } : { x: '10vw', opacity: 0 }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
         >
-        <h4 className="contentTitle">Contact Information</h4>
+            <h4 className="contentTitle">Contact Information</h4>
             <div className="personalContactInfo">
                 <div className="infoType">Name</div>
                 {personalDetails.name}
@@ -182,22 +186,18 @@ const ContactPage = ({personalDetails}) => {
 
             <hr/>
 
-            <motion.div 
-            ref={ref} 
-            initial='hidden' 
-            animate={inView ? 'visible' : 'hidden'} 
-            variants={fadeInVariants}>
-
-                <SocialLinks/>
-                <div className='cv-section'>
-                    <button className='btn' onClick={handleDownload}>Download CV</button>
+            <motion.div ref={ref} initial="hidden" animate={inView ? 'visible' : 'hidden'} variants={fadeInVariants}>
+                <SocialLinks />
+                <div className="cv-section">
+                    <button className="btn" onClick={handleDownload}>
+                    Download CV
+                    </button>
                 </div>
-
             </motion.div>
         </motion.div>
         </section>
-        </div>
-    )
+    </div>
+    );
 };
 
-export default ContactPage
+export default ContactPage;
